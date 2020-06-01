014a9e8bb5b508259e2d26cc94bc00a224a83b71
IlyaKirsanov <ilya.kirsanov@gmail.com>
2020-06-01 17:20:32 +0300

    fetch and event listener with timeout added to prevent api fetch every keypress

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-const fetchData = async () =>{
+const fetchData = async (searchTerm) =>{
   const response = await axios.get('http://www.omdbapi.com/',{
     params: {
       apikey:'aeaeb0c9',
-      s:'avengers'
+      s: searchTerm
     }
   });
 
   console.log(response.data)
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+const debounce = (func, delay = 1000)=>{
+  let timeoutId;
+
+  return (...args)=>{
+    if(timeoutId){
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(()=>{
+      func.apply(null, args);
+    },delay)
+  }
+};
+
+const onInput = debounce((event) =>{
+  fetchData(event.target.value)
+},1000);
+
+input.addEventListener('input', onInput);
